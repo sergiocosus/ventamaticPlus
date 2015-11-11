efea4e1066ce5b3e9c7b51f7f6e91bbab08b5561
Sergio Contreras Sustaita <sergio.contreras@triolabs.mx>
2015-11-10 23:14:10 -0600

    Now each ten seconds update the graphic

--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -3,47 +3,62 @@
 @section('content')
     <script>
         Ventamatic.controller("LineCtrl", function ($scope, UserSession) {
-            UserSession.get().then(function(sessions){
-                var current = moment().hour(0).minute(0).second(0);
-                var last = moment(current).add(1,'day');
+            $scope.data = null;
+            $scope.updateChart = function(sessions,period, subPeriod, format){
+                var current = moment().startOf(period);
+                var last = moment(current).add(1,period);
 
                 var data=[];
                 var labels=[];
                 var i= 0;
                 var j=0;
-                while(!current.isSame(last,'hour')){
-                    labels[i]=moment(current).format('hh:mm a');
 
+                while(!current.isSame(last,subPeriod)){
+                    labels[i]=moment(current).format(format);
                     data[i] = 0;
+
                     while(j < sessions.length) {
                         var sessionMoment =moment.utc(sessions[j].created_at);
-                        if(sessionMoment.isSame(current,'hour')) {
+                        if(sessionMoment.isSame(current,subPeriod)) {
                             data[i]++;
                             j++;
                             continue;
                         }
-                        if(sessionMoment.isBefore(current,'hour')){
+                        if(sessionMoment.isBefore(current,subPeriod)){
                             j++;
                         }else{
                             break;
                         }
                     }
-                    current.add(1,'hour');
+                    current.add(1,subPeriod);
                     i++;
-                    if(i>= 100){
-                        break;
-                    }
+                    /* if(i>= 100){
+                     break;
+                     }*/
                 }
 
                 $scope.labels = labels;
-                $scope.series = ['Series A'];
+                $scope.series = ['Visitas'];
                 $scope.data = [
                     data
                 ];
                 $scope.onClick = function (points, evt) {
                     console.log(points, evt);
                 };
-            });
+            };
+
+            $scope.updateData = function(){
+                console.log("updating");
+                UserSession.get().then(function(sessions){
+                    $scope.data = sessions;
+                    $scope.updateChart($scope.data, 'day','hour','YYYY-MM-dd hh:mm a');
+                });
+            };
+            setInterval(function(){
+                $scope.updateData();
+            },5000);
+
+            $scope.updateData();
 
         });
     </script>
